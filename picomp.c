707a4c6fccfc272cdb39068aff085b87330dd56d
Panagiotis Stamatopoulos <takis@di.uoa.gr>
2025-10-20 11:41:04 +0300

    Increased precision

--- picomp.c
+++ picomp.c
@@ -13,7 +13,7 @@ int main()
 		current = 1/(((double) i)*((double) i));          /* Current term */
 		sum = sum+current;                     /* Add current term to sum */
 		i++;                                             /* Next term now */
-	} while (current > 1.0e-15);  /* Stop if current term is very small */
+	} while (current > 1.0e-18);  /* Stop if current term is very small */
 	pi = sqrt(6*sum);                    /* Compute approximation of pi */
 	printf("Summed %d terms, pi is %.8f\n", i-1, pi);
 	return 0;
